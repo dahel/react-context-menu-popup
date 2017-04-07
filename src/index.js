c2b8a7496609cae8c53b534a0747da417ba52d08
Sebastian Prymas <sebastian.prymas@symetria.pl>
2017-04-07 14:34:34 +0200

    next

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ContextMenuPopup from './components/ContextMenuPopup';
+import ContextMenuPopupOption from './components/ContextMenuPopupOption';
+import ContextMenuPopupTrigger from './components/ContextMenuPopupTrigger';
+import ContextMenuPopupOptions from './components/ContextMenuPopupOptions';
+
+export {
+	ContextMenuPopup,
+	ContextMenuPopupTrigger,
+	ContextMenuPopupOptions,
+	ContextMenuPopupOption
+};
